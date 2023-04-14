ab358e92925fb064b5730615478ac697c51788a6
blank <17322619133@163.com>
2023-04-14 14:21:34 +0800

    fix: replace req.body.uuid with signed cookie

--- public/js/init.js
+++ public/js/init.js
@@ -7,27 +7,24 @@ $(() => {
 			data: {
 				uuid
 			},
-
 			dataType: "json",
 			success: function (result) {
-				// console.log(result);
+				console.log(result);
+				KEY = '';
+				sessionStorage.clear();
+
+				const cookies = document.cookie.split(";");
+
+				for (let i = 0; i < cookies.length; i++) {
+					const cookie = cookies[i];
+					const eqPos = cookie.indexOf("=");
+					const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+					document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+				}
 			},
 			error: function (error) {
 				// console.error(error.responseText);
 			}
 		});
-
-		KEY = '';
-		sessionStorage.clear();
-
-		const cookies = document.cookie.split(";");
-
-		for (let i = 0; i < cookies.length; i++) {
-			const cookie = cookies[i];
-			const eqPos = cookie.indexOf("=");
-			const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-			document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-		}
-
 	}
 })
